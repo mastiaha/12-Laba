5f0b89deee465dc6bb00cd0d8a0f691d232d907c
mastiaha <mastiaha@mail.ru>
2022-01-06 17:35:13 +0300

    Добавьте файлы проекта.

---
+++ 12 Laba/Krug.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_Laba
+{
+    static class Krug
+    {
+        static public double GetPerimetr(int r)
+        {
+            return 2 * r * Math.PI;
+        }
+        static public double GetPloshad(int r)
+        {
+            return Math.Pow(r, 2) * Math.PI;
+        }
+        static public double GetGipotenuza(int a, int b, int r)
+        {
+            double c = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
+            if (r >= c)
+            {
+                Console.WriteLine("Точка принадлежит кругу с радиусом {0}", r);
+                return c;
+                
+            }
+            else
+            {
+                Console.WriteLine("Точка не принадлежит кругу с радиусом {0}", r);
+                return c;
+            }
+            
+        }
+    }
+}
---
+++ 12 Laba/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_Laba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите радиус круга");
+            int r = Convert.ToInt32(Console.ReadLine());
+            double perimetr = Krug.GetPerimetr(r);
+            double ploshad = Krug.GetPloshad(r);
+            Console.WriteLine("Введите координаты точки");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            double c = Krug.GetGipotenuza(a, b, r);
+            
+            Console.WriteLine($"{perimetr} \n{ploshad}");
+            Console.ReadKey();
+            Console.WriteLine("Для выхода из программы нажмите любую клавишу");
+        }
+    }
+}
